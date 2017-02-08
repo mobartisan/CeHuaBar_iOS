f2f433615b0566b56d504637123abb759c98bbe0
曹兴星 <1104190972@qq.com>
2017-02-08 16:23:54 +0800

    fix bug

--- TeamTiger/Manager/MessageManager/MessageManager.m
+++ TeamTiger/Manager/MessageManager/MessageManager.m
@@ -223,28 +223,6 @@ static MessageManager *singleton = nil;
     NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:msgObj options:kNilOptions error:nil];
     NSLog(@"%@",dict);
     
-<<<<<<< HEAD
-#warning  to do handle messages and optimize cod
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        //show message
-        if ([UserDefaultsGet(ALLOW_USER_KEY_SHOW_MESSAGE) integerValue] == 1) {
-            AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
-            STPushModel *model = [[STPushModel alloc] init];
-            model.content = @"您有一条新消息！";
-            appDelegate.topView.model = model;
-            [appDelegate displayPushView];
-        }
-        //play shake
-        if ([UserDefaultsGet(ALLOW_USER_KEY_PLAY_SHAKE) integerValue] == 1) {
-            AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
-        }
-        //play audio
-        if ([UserDefaultsGet(ALLOW_USER_KEY_PLAY_AUDIO) integerValue] == 1) {
-            static SystemSoundID soundIDTest = 0;
-            NSString *path = [[NSBundle mainBundle] pathForResource:@"bbs" ofType:@"caf"];
-            if (path) {
-                AudioServicesCreateSystemSoundID((__bridge CFURLRef)[NSURL fileURLWithPath:path], &soundIDTest);
-=======
 #warning  to do handle messages and optimize code
     
     NSTimeInterval nowTimeInterval = [NSDate date].timeIntervalSince1970;
@@ -257,7 +235,6 @@ static MessageManager *singleton = nil;
                 model.content = @"您有一条新消息！";
                 appDelegate.topView.model = model;
                 [appDelegate displayPushView];
->>>>>>> origin/master
             }
             //play shake
             if ([UserDefaultsGet(ALLOW_USER_KEY_PLAY_SHAKE) integerValue] == 1) {
