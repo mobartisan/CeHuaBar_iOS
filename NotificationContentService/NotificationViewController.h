6700874894e9bc2d6ea7010f044aaed4886fbbf7
曹兴星 <1104190972@qq.com>
2017-02-10 16:46:06 +0800

    fix bug

---
+++ NotificationContentService/NotificationViewController.h
@@ -0,0 +1,13 @@
+//
+//  NotificationViewController.h
+//  NotificationContentService
+//
+//  Created by xxcao on 2017/2/10.
+//  Copyright © 2017年 MobileArtisan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotificationViewController : UIViewController
+
+@end
---
+++ NotificationContentService/NotificationViewController.m
@@ -0,0 +1,45 @@
+//
+//  NotificationViewController.m
+//  NotificationContentService
+//
+//  Created by xxcao on 2017/2/10.
+//  Copyright © 2017年 MobileArtisan. All rights reserved.
+//
+
+#import "NotificationViewController.h"
+#import <UserNotifications/UserNotifications.h>
+#import <UserNotificationsUI/UserNotificationsUI.h>
+
+@interface NotificationViewController () <UNNotificationContentExtension>
+
+@property IBOutlet UILabel *titleLabel;
+@property IBOutlet UILabel *subtitleLabel;
+@property IBOutlet UILabel *bodyLabel;
+@property IBOutlet UIImageView *bgImgV;
+
+@end
+
+@implementation NotificationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any required interface initialization here.
+}
+
+- (void)didReceiveNotification:(UNNotification *)notification {
+    self.titleLabel.text = [NSString stringWithFormat:@"%@", notification.request.content.title];
+    self.subtitleLabel.text = [NSString stringWithFormat:@"%@", notification.request.content.subtitle];
+    self.bodyLabel.text = [NSString stringWithFormat:@"%@", notification.request.content.body];
+
+    UNNotificationAttachment *attachment = notification.request.content.attachments.firstObject;
+    if (attachment) {
+        if ([attachment.URL startAccessingSecurityScopedResource]) {
+            self.bgImgV.image = [UIImage imageWithContentsOfFile:attachment.URL.path];
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                [attachment.URL stopAccessingSecurityScopedResource];
+            });
+        }
+    }
+}
+
+@end
