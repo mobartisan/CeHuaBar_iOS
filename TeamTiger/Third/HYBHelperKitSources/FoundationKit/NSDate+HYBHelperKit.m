17e6eb782d48fc7029f8dd7bc4410cc2c44bc3f7
曹兴星 <1104190972@qq.com>
2017-02-16 15:46:39 +0800

    fix bug

--- TeamTiger/Third/HYBHelperKitSources/FoundationKit/NSDate+HYBHelperKit.m
+++ TeamTiger/Third/HYBHelperKitSources/FoundationKit/NSDate+HYBHelperKit.m
@@ -440,8 +440,13 @@
   NSTimeInterval retTime = 1.0;
   if (time < 3600) { // 小于一小时
     retTime = time / 60;
-    retTime = retTime <= 0.0 ? 1.0 : retTime;
-    return [NSString stringWithFormat:@"%.0f分钟前", retTime];
+      if (retTime < 1) {
+          return @"刚刚";
+      } else {
+          return [NSString stringWithFormat:@"%.0f分钟前", retTime];
+      }
+//    retTime = retTime <= 0.0 ? 1.0 : retTime;
+//    return [NSString stringWithFormat:@"%.0f分钟前", retTime];
   } else if (time < 3600 * 24) { // 小于一天，也就是今天
     retTime = time / 3600;
     retTime = retTime <= 0.0 ? 1.0 : retTime;
