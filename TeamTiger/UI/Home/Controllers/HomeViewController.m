64ead4e422803f2da01fedafee85edeb32b4bc2b
cjdale <cjdale@163.com>
2017-03-14 09:40:49 +0800

    commit code

--- TeamTiger/UI/Home/Controllers/HomeViewController.m
+++ TeamTiger/UI/Home/Controllers/HomeViewController.m
@@ -739,8 +739,8 @@
 #pragma mark - 分组或者项目Moments
 - (void)handleConvertId:(NSNotification *)notification {
     self.imageView.image = kImage(@"img_cover");
-    [self.dataSource removeAllObjects];
-    [self.tableView reloadData];
+//    [self.dataSource removeAllObjects];
+//    [self.tableView reloadData];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         NSDictionary *parameterDic = nil;
         BOOL isLoading = NO;
